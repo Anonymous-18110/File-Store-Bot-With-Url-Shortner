56c809de1fb8ad62e57d730826e32c44b5c49814
Anonymous-18110 <100400779+Anonymous-18110@users.noreply.github.com>
2023-11-10 14:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,3 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
-app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, 8080).start()
