53658dfc43d6aa7a86d52c765afcaff96b2f2c06
Anonymous-18110 <100400779+Anonymous-18110@users.noreply.github.com>
2023-11-10 14:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from aiohttp import web
 
-@app.route('/')
-def hello_world():
-    return 'TechVJ'
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("DQTheFileDonor")
 
 
-if __name__ == "__main__":
-    app.run()
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
 
+app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, 8080).start()
